789efb4c3569690784d63b555a30cfc3c4c08baa
ericsalesdeandrade <sdaeric19@gmail.com>
2024-02-28 09:01:15 +0000

    init code

---
+++ string_manipulator/string_manipulator.py
@@ -0,0 +1,18 @@
+"""
+Build a String Manipulator with the following requirements
+- convert to lower case
+- convert to upper case
+- reverse the string
+- remove a defined pattern from the string
+- find a defined substring
+- remove whitespaces from the string
+- make sure the string is not empty
+- capitalize the first letter
+- capitalize the first letter of every word
+- check if the string is a palindrome or not
+"""
+
+
+class StringManipulator:
+    def to_lower_case(self, mystring: str):
+        return mystring.lower()
