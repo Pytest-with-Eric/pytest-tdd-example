ee4f4d69f8d64ace5294b99025a11f1d6df26905
ericsalesdeandrade <sdaeric19@gmail.com>
2024-03-06 08:07:22 +0000

    Update string_manipulator.py

--- string_manipulator/string_manipulator.py
+++ string_manipulator/string_manipulator.py
@@ -1,15 +1,7 @@
 """
 Build a String Manipulator with the following requirements
 - convert to lower case
-- convert to upper case
-- reverse the string
 - remove a defined pattern from the string
-- find a defined substring
-- remove whitespaces from the string
-- make sure the string is not empty
-- capitalize the first letter
-- capitalize the first letter of every word
-- check if the string is a palindrome or not
 """
 
 
@@ -22,10 +14,10 @@ class StringManipulator:
         return my_string.lower()
 
     def remove_pattern(self, my_string: str, pattern: str):
-        if pattern not in my_string:
-            return "Pattern not found"
         if not my_string:
             return "String is empty"
+        if pattern not in my_string:
+            return "Pattern not found"
         if not isinstance(my_string, str):
             return "Invalid input"
         new_string = my_string.replace(pattern, "")
